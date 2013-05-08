49f2946e8901fce26b96fe3aeceae03cf210ee4a
averangeall <averangeall@gmail.com>
2013-05-08 14:49:13 +0800

    Fix background page problem

--- js/background.js
+++ js/background.js
@@ -1,20 +1,21 @@
 function gotoLatestGag() {
     chrome.tabs.create({url: 'http://9gag.com/gag/6894177'});
-    updateGotoGag();
 }
 
 function updateGotoGag() {
-    chrome.browserAction.onClicked.removeListener(gotoLatestGag);
-    chrome.browserAction.onClicked.addListener(gotoLatestGag);
+    if(!chrome.browserAction.onClicked.hasListener(gotoLatestGag))
+        chrome.browserAction.onClicked.addListener(gotoLatestGag);
 }
 
 function decideAction(tabId, changeInfo, tab) {
+    updateGotoGag();
     if(is9gag(tab.url) && getGagId(tab.url) != null) {
+        chrome.browserAction.onClicked.removeListener(gotoLatestGag);
         chrome.browserAction.setPopup({tabId: tab.id, popup: 'popup.html'});
         chrome.browserAction.setIcon({path: 'images/icon-19.png', tabId: tabId});
-    } else
-        updateGotoGag();
+    }
 }
 
-chrome.tabs.onUpdated.removeListener(decideAction);
-chrome.tabs.onUpdated.addListener(decideAction);
+if(!chrome.tabs.onUpdated.hasListener(decideAction))
+    chrome.tabs.onUpdated.addListener(decideAction);
+
