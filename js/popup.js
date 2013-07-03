3e50afc283f20d85226c384cd952fef7fc4bd549
averangeall <averangeall@gmail.com>
2013-07-03 15:05:22 +0800

    Filter user input and get explains by user input

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,3 @@
-//
-//function userRecomm() {
-//    if($.trim(input) != '')
-//        putSingleRecomm(input);
-//}
-//
-//function setRecommBtnClass() {
-//    $.each($('#recomm-words p a'), function(idx) {
-//        if(idx == 0)
-//            $(this).attr('class', 'btn btn-large btn-info')
-//    });
-//}
-//
-//function setInputListener() {
-//    $('#input').keyup(function(evt) {
-//        input = $('#input').val();
-//        if(evt.which != 13) {
-//            filterRecomm();
-//            userRecomm();
-//            setRecommBtnClass();
-//        } else {
-//            $.each($('#recomm-words p a'), function(idx) {
-//                if(idx == 0)
-//                    putAllDefi($(this).html());
-//            });
-//        }
-//    });
-//}
-
 $(function() {
     chrome.windows.getCurrent(function(w) {
         chrome.tabs.getSelected(w.id, function (response){
@@ -41,7 +12,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     gagId = request.gag_id;
     putTitle(request.title);
     putAllRecomms(gagId);
-    //setInputListener(gag_id);
+    setInputListener();
     focusInput();
 });
 
