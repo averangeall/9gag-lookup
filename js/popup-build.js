4cc512dd88ced922d8f73e29d5a44283752c2a1d
averangeall <averangeall@gmail.com>
2013-08-08 11:16:06 +0800

    Add recomm buttons

--- js/popup-build.js
+++ js/popup-build.js
@@ -3,13 +3,24 @@ function buildMain() {
     var main = $('<div/>').addClass('lookup-popup-main');
     var header = $('<div/>').attr('id', 'header');
     var navs = $('<div>').addClass('lookup-nav')
-                         .append($('<a/>').attr('href', '#').html('字典'))
+                         .append($('<a/>').attr('href', 'javascript: void(0);').html('字典'))
                          .append(' ')
-                         .append($('<a/>').attr('href', '#').html('通知'))
+                         .append($('<a/>').attr('href', 'javascript: void(0);').html('通知'))
                          .append(' ')
-                         .append($('<a/>').attr('href', '#').html('個人'));
+                         .append($('<a/>').attr('href', 'javascript: void(0);').html('個人'));
+    var recomms = $('<p/>').addClass('lookup-recomm')
+                           .append($('<a/>').addClass('btnn btnn-large btnn-primary').attr('href', 'javascript: void(0);').html('Immature'))
+                           .append(' ')
+                           .append($('<a/>').addClass('btnn btnn-large btnn-primary').attr('href', 'javascript: void(0);').html('Please'))
+                           .append(' ')
+                           .append($('<a/>').addClass('btnn btnn-large btnn-primary').attr('href', 'javascript: void(0);').html('ConfessionBear'))
+                           .append(' ')
+                           .append($('<a/>').addClass('btnn btnn-large btnn-primary').attr('href', 'javascript: void(0);').html('Pretending'));
+    var keywords = $('<div/>').append($('<span/>').addClass('lookup-heading').html('關於這篇有這些關鍵字喔'))
+                              .append(recomms);
     header.append(navs);
     main.append(header);
+    main.append(keywords);
     contain.append(main);
     return contain;
 }
